123da2ac6ba8f0290dd6b2ef8eac018bba1c231d
Hanna Möllergren <hannamollergren@gmail.com>
2020-05-14 08:53:22 +0200

    Updates

---
+++ precache-manifest.dfc69c1004916d84bfb39c858c3bede7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb3efea7cd443dab5c45ec85af3708d",
+    "url": "/React-Star-Wars/index.html"
+  },
+  {
+    "revision": "63e06730180922e31dba",
+    "url": "/React-Star-Wars/static/css/main.f3df0d0e.chunk.css"
+  },
+  {
+    "revision": "c479bb3825d995f5452e",
+    "url": "/React-Star-Wars/static/js/2.80ea1054.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Star-Wars/static/js/2.80ea1054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e06730180922e31dba",
+    "url": "/React-Star-Wars/static/js/main.5d23f56c.chunk.js"
+  },
+  {
+    "revision": "87c04113677d44f3343e",
+    "url": "/React-Star-Wars/static/js/runtime-main.878d0575.js"
+  },
+  {
+    "revision": "0df16aa1b4c67aadc835d5889321eca9",
+    "url": "/React-Star-Wars/static/media/30157.0df16aa1.jpg"
+  },
+  {
+    "revision": "475b70942556db139ff77efac2093c05",
+    "url": "/React-Star-Wars/static/media/Image1.475b7094.svg"
+  }
+]);
